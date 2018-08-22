8b854a9ee98a306f4200aa1fc2d50b0932696bb7
AbdelG <agenao1997@gmail.com>
2018-08-21 23:51:40 -0400

    Movement Script

---
+++ Assets/movimiento.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimiento : MonoBehaviour {
+
+    // Use this for initialization
+    public float speed;
+    public float jump;
+    public GameObject palomo;
+    //public float rayCheckDistance;
+    Rigidbody2D rb;
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(x * speed * Time.deltaTime, 0, 0);
+        float y = Input.GetAxis("Vertical");
+        transform.position += new Vector3(0, y * jump * Time.deltaTime, 0);
+        
+        
+    }
+}
