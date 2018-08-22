63bdc98c5aecb8d057a42a3d7c332fdf9705f134
AbdelG <agenao1997@gmail.com>
2018-08-22 01:28:58 -0400

    Other shit

---
+++ Assets/CameraMov.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMov : MonoBehaviour {
+    public GameObject palomo;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - palomo.transform.position;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = palomo.transform.position + offset;
+    }
+}
